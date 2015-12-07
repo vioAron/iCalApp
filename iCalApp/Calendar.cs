96b067b59a8e536fec074e2692e9c354106a7aee
Viorel Aron <viorel.aron@ullink.com>
2015-12-07 20:04:39 +0200

    Initial commit

---
+++ iCalApp/Calendar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DDay.iCal;
+
+namespace iCalApp
+{
+    public class Calendar
+    {
+        public string CreateEvent(DateTime when, string eventDescription)
+        {
+            iCalendar calendar = new iCalendar();
+            calendar.AddProperty(new CalendarProperty());
+        }
+    }
+}
---
+++ iCalApptTests/CalendarTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NUnit.Framework;
+using iCalApp;
+
+namespace iCalApptTests
+{
+    public class CalendarTests
+    {
+        [Test]
+        public void GivenAnCalendar_WhenAddingAnEvent_ThenTheicsTextIsCorrect()
+        {
+            Calendar calendar = new Calendar();
+            DateTime when = DateTime.Today;
+            string eventDescription = "ShipIt";
+            Assert.That(calendar.CreateEvent(when, eventDescription), Is.EqualTo("aaaaaaaaa"));
+        }
+    }
+}
